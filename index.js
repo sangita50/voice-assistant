ced30a931682e4919588b310b5d12b837c60d928
imsourav19 <souravgupta8068@gmail.com>
2024-05-05 23:53:36 +0530

    updated

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ import OpenAI from "openai";
 // import cors from 'cors';
 
 
-import serverless from "serverless-http";
-const router = express.Router();
-
-
 const app = express();
 // app.use(cors());
 // this part may raise error is executed in different environment
@@ -40,22 +36,18 @@ async function main(input) {
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname1, "index.html"));
 });
 
-router.post("/api", async function (req, res, next) {
+app.post("/api", async function (req, res, next) {
   console.log(req.body);
   const mes = await main(req.body.input);
   res.json({ success: true, message: mes });
 });
 
+app.listen(port, () => {
+  console.log("Running...");
+});
 
-app.use("/.netlify/functions/app", router);
-export default serverless(app);
-
-// app.listen(port, () => {
-//     console.log("Running...");
-//   });
-  
 // "test": "echo \"Error: no test specified\" && exit 1"
